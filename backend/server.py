0513df7799605a586cb3779f159c9948c38abf17
Ömer Faruk Günay <116501544+ofarukgunay@users.noreply.github.com>
2025-07-07 01:33:03 +0300

    Update

--- backend/server.py
+++ backend/server.py
@@ -16,7 +16,7 @@ load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url, tls=True, tlsAllowInvalidCertificates=True)
+client = AsyncIOMotorClient(mongo_url, tlsAllowInvalidCertificates=True)
 db = client[os.environ['DB_NAME']]
 collection = db["status_checks"]  # Koleksiyon referansı
 
